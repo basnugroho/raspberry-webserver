2a32d1c6a6a9ea9edc7410fb1fa36129c8267f85
Baskoro Nugroho <bas.nugroho@yahoo.com>
2021-10-31 03:09:04 +0000

    fixing blueprint

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from speedtest import speedtest_bp
-from flt import flt_bp
+#from flt import flt_bp
 
 app = Flask(__name__)
-app.register_blueprint(speedtest_bp, url_prefix='/speedtest')
-app.register_blueprint(flt_bp, url_prefix='/flt')
 
-app.run(host='0.0.0.0', port=443)
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    return "Siap!"
+
+app.register_blueprint(speedtest_bp, url_prefix='/')
+# app.register_blueprint(flt_bp, url_prefix='/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=443)
